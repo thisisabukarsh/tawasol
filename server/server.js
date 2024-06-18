50e050be8057ddf11bfbd25b73a6c934f2498df6
thisisabukarsh <155810138+thisisabukarsh@users.noreply.github.com>
2024-06-18 16:53:35 +0300

    register api & user model

--- server/server.js
+++ server/server.js
@@ -2,8 +2,9 @@ const express = require("express");
 const connectDB = require("./config/db");
 
 const app = express();
+app.use(express.json());
 
-app.use("/api/users", require("./routes/user"));
+app.use("/api/users", require("./routes/users"));
 app.use("/api/posts", require("./routes/posts"));
 app.use("/api/profile", require("./routes/profile"));
 app.use("/api/auth", require("./routes/auth"));
